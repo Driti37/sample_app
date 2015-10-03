f6c7732e8dfc31119cbd0922682b7b646bedcc8c
Deepti <deeptisheth1@gmail.com>
2015-10-03 21:00:35 +0530

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   match '/signout', to: 'sessions#destroy', :via => [:delete]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'static_pages#home'
 
